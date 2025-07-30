42d2741bb509143b35a5172be91d5e45147da2dc
bcheban <128683111+bcheban@users.noreply.github.com>
2025-07-30 11:00:48 +0300

    changed resume

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/1DrQgreBbu_dC66PiJPTlqL6rKFcxpuxT/view"
+                href="https://drive.google.com/file/d/1DrQgreBbu_dC66PiJPTlqL6rKFcxpuxT/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 View Resume
